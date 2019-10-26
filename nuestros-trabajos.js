b0e75e7430f0385685dd9d931d671a54c1538d50
salmeril <salmerilucas@gmail.com>
2019-10-26 13:14:49 -0300

    entrega

---
+++ nuestros-trabajos.js
@@ -0,0 +1,15 @@
+console.dir(document);
+
+
+
+var div1=document.getElementsByClassName("")
+
+
+
+var MostrarLista1=document.getElementsByClassName("div1");-
+mostrarLista.addEventListener("hover", function(){
+    // lista.classList.add("active")
+    MostrarLista1.classList.toggle("active");   
+})
+
+// hacer
